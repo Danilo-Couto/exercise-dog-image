fb0c2efeb49d2d6555aa8d9329ae1eb635a583a6
Danilo Couto <couto.danilo95@gmail.com>
2021-12-14 18:52:38 -0300

    dogimage, completo

--- src/App.js
+++ src/App.js
@@ -4,39 +4,86 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      dogs: '',
+      dogs: {},
+      dogName: '',
+      arrayDogsName: [],
     };
   }
 
-  fetchdogs = () => {
+  fetchdog = () => {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then(response => response.json())
-      .then(results => {
-
+      .then(results => { 
       this.setState({
-        dogs: results
+        dogs: results,
       })
     })
   }
-
+ 
   componentDidMount() {
-    this.fetchdogs();
+    if (localStorage.namedDogURL) {
+      const parseStorage = JSON.parse(localStorage.namedDogURL);
+      const lastDog = parseStorage[parseStorage.length - 1].message;
+      this.setState({
+        arrayDogsName: parseStorage,
+        dogs: { message: lastDog }
+      });
+    } else {
+      this.fetchdog();
+    }
+  }
+
+  shouldComponentUpdate = (actualProps, nextState) => {
+    if (nextState.dogs.message.includes('terrier')) {
+      return false 
+      } 
+      return true
+   }
+
+   componentDidUpdate = ()  => {
+    localStorage.setItem('lastSavedDog', this.state.dogs.message)
+    const dogBreed = this.state.dogs.message.split('/')[4]
+    // alert(dogBreed)
+    return true
+  }
+
+   saveDogName = () =>{
+    const { dogs: {message}, dogName, arrayDogsName } = this.state;
+    const dogData = {message, dogName }
+    const arrayUrleNames = [...arrayDogsName, dogData]
+    this.setState ({arrayDogsName:  arrayUrleNames});
+    this.setState ({dogName: '' }) 
+    localStorage.setItem("namedDogURL", JSON.stringify(arrayUrleNames));
   }
 
   render() {
     const { dogs } = this.state;
-    
-    if (dogs === '') {
-      console.log('loading')
-      return 'LOADING...'    
-    }
+
     return (
-      <div>
-        <h1>
-          Dogs
-        </h1>
+      !dogs ? 'LOADING...' :   
+        <div>
+        <h1> Dogs </h1>
+        <input 
+          name='dogName'
+          type='text'
+          value={this.dogName}
+          placeholder = 'nomeie este dog'
+          onChange = {({target})=> {
+            this.setState({
+              dogName: {[target.name]: target.value}
+            })
+          }}
+        />
+        <button
+          onClick = {this.saveDogName}
+        > Salvar Nome do Dog
+        </button>
+        <button
+          type='button'
+          onClick={this.fetchdog}
+        > Próximo dog
+        </button>
       <div>
         <img className='img'
           src={dogs.message}
@@ -44,12 +91,6 @@ class App extends Component {
         >
       </img>
       </div>
-        <button
-          type='button'
-          onClick={this.fetchdogs}
-        >
-          Próximo dog
-        </button>
       </div>
     );
   }
